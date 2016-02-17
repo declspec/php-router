6237b56b92340b6eb28ac6c02dd65b231a87cd6c
Jason Larke <jasonlarke@live.com.au>
2016-02-17 16:52:55 +0800

    Updated example page

--- index.php
+++ index.php
@@ -1,25 +1,32 @@
 <?php
 require("src/router.php");
 
-function loggingMiddleware($req) {
-    echo "<!--\n";
-    var_dump($req);
-    echo "\n-->\n";
-    
-    return false;
-}
-
-$baseUrl = substr(__DIR__, strlen($_SERVER["DOCUMENT_ROOT"]));
-
 $starttime = microtime(true);
 
-$app = new Router($baseUrl);
+$router = new Router("/php-router");
 
-$app->get("/:param", 'loggingMiddleware', function($req) {
-    echo "<h1>Not Index</h1>"; 
+// Define the index
+$router->all('/', function($req, $res) {
+    throw new Exception("Bye, world");
+    $res->send('<h1>Hello world, from php-router</h1>');
 });
 
-$app->run();
+// Define a 404 handler as the last regular route
+$router->all('*', function($req, $res) {
+    $res->status(404)->send('<h1>Not Found</h1>');
+});
+
+// Define an error handler for any exceptions that occur in the rotues
+$router->error(function($ex, $req, $res) {
+    $html = "<h1>Internal Server Error</h1>\n" .
+    	"<strong>" . htmlentities($ex->getMessage(), ENT_QUOTES, "UTF-8") . "</strong>\n" .
+        "<pre><code>" . htmlentities($ex->getTraceAsString(), ENT_QUOTES, "UTF-8") . "</code></pre>";
+    
+    $res->status(500)->send($html);
+});
+
+$router->run();
+
 $elapsed = round(microtime(true) - $starttime, 4);
 
 echo "<!-- Page generated in {$elapsed} seconds -->";
